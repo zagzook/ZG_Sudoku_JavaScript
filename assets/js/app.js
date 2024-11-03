cb223993df6c1827d18dcbe2af3c0cbc673b735c
zagzook <ja0213@gmail.com>
2024-11-03 04:05:19 -0500

    Added the ability to get boards from a flat file, also added the ability to add tiles to the board but have not updated the keyboard to add the tile instead of number

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
 const startScreen = document.querySelector('#start-screen')
 const gameScreen = document.querySelector('#game-screen')
 const pauseScreen = document.querySelector('#pause-screen')
+const resultScreen = document.querySelector('#result-screen')
 const darkModeToggle = document.querySelector('#dark-mode-toggle')
 const metaNameThemeColor = document.querySelector('meta[name="theme-color"]')
-const btnContinue = document.querySelector('#btn-continue')
 const nameInput = document.querySelector('#input-name')
+const btnContinue = document.querySelector('#btn-continue')
 const btnPlay = document.querySelector('#btn-play')
 const btnLevel = document.querySelector('#btn-level')
 const btnPause = document.querySelector('#btn-pause')
@@ -16,11 +17,14 @@ const numberKeys = document.querySelector('.numbers')
 const playerName = document.querySelector('#player-name')
 const gameLevel = document.querySelector('#game-level')
 const gameTimer = document.querySelector('#game-time')
+const resultTime = document.querySelector('#result-time')
+const tilePath = 'assets/images/tiles/'
 
 //add dark/light theme toggle
 darkModeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark')
     const isDarkMode = document.body.classList.contains('dark')
+    themeMode = isDarkMode ? 'dark' : 'light'
     localStorage.setItem('darkmode', isDarkMode)
     // change mobile status bar color
     metaNameThemeColor.setAttribute('content', isDarkMode ? '#1a1a2e' : '#fff')
@@ -28,63 +32,25 @@ darkModeToggle.addEventListener('click', () => {
 //------------------------------
 
 //initial value
+let themeMode = 'dark'
+let cells = undefined
+let numberInputs = undefined
 let level_index = 0
 let level = CONSTANT.LEVEL[level_index]
 let timer = null
 let pause = false
 let seconds = 0
-
-const getGameInfo = () => JSON.parse(localStorage.getItem('game'))
+let su = undefined
+let suAnswer = undefined
+let selectedCell = -1
 //-----------
-
-// add space for each 9 cells
-function initGameGrid() {
-    const cells = document.querySelectorAll('.main-grid-cell')
-
-    for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
-        let row = Math.floor(i / CONSTANT.GRID_SIZE)
-        let col = i % CONSTANT.GRID_SIZE
-        if (row === 2 || row === 5) cells[i].style.marginBottom = '10px'
-        if (col === 2 || col === 5) cells[i].style.marginRight = '10px'
-
-    }
-}
-
-//---------------------------
+const getGameInfo = () => JSON.parse(localStorage.getItem('game'))
 
 const setPlayerName = (name) => localStorage.setItem('playerName', name)
+
 const getPlayerName = () => localStorage.getItem('playerName')
+
 const showTime = (seconds) => new Date(seconds * 1000).toISOString().substring(11, 19)
-
-
-const startGame = () => {
-    startScreen.classList.remove('active')
-    gameScreen.classList.add('active')
-
-    playerName.innerHTML = nameInput.value.trim()
-    setPlayerName(nameInput.value.trim())
-
-    gameLevel.innerHTML = CONSTANT.LEVEL_NAME[level_index]
-
-    showTime(seconds)
-
-    timer = setInterval(() => {
-        if (!pause) {
-            seconds = seconds + 1
-            gameTimer.innerHTML = showTime(seconds)
-        }
-    }, 1000)
-}
-
-const retunrStartScreen = () => {
-    clearInterval(timer)
-    pause = false
-    seconds = 0
-    startScreen.classList.add('active')
-    gameScreen.classList.remove('active')
-    pauseScreen.classList.remove('active')
-    gameTimer.innerHTML = '00:00:00'
-}
 
 // add button events
 btnLevel.addEventListener('click', (e) => {
@@ -95,6 +61,7 @@ btnLevel.addEventListener('click', (e) => {
 
 btnPlay.addEventListener('click', () => {
     if (nameInput.value.trim().length > 0) {
+        initSudoku()
         startGame()
 
     } else {
@@ -117,7 +84,8 @@ btnResume.addEventListener('click', () => {
 })
 
 btnNewGame.addEventListener('click', () => {
-    retunrStartScreen()
+    clearSudoku()
+    returnStartScreen()
 })
 
 
@@ -134,9 +102,11 @@ function init() {
     btnContinue.style.display = game ? 'grid' : 'none'
 
     createGridBoard()
+    cells = document.querySelectorAll('.main-grid-cell')
     createNumberBoard()
     initGameGrid()
-
+    initCellsEvent()
+    initNumberInputEvent()
     if (getPlayerName()) {
         nameInput.value = getPlayerName()
     } else {
@@ -145,9 +115,29 @@ function init() {
 
 }
 
+function startGame() {
+    startScreen.classList.remove('active')
+    gameScreen.classList.add('active')
+
+    playerName.innerHTML = nameInput.value.trim()
+    setPlayerName(nameInput.value.trim())
+
+    gameLevel.innerHTML = CONSTANT.LEVEL_NAME[level_index]
+
+    seconds = 0
+    showTime(seconds)
+    timer = setInterval(() => {
+        if (!pause) {
+            seconds = seconds + 1
+            gameTimer.innerHTML = showTime(seconds)
+        }
+    }, 1000)
+}
+
 function createGridBoard() {
     for (let i = 0; i < CONSTANT.GRID_SIZE * CONSTANT.GRID_SIZE; i++) {
         const square = document.createElement('div')
+
         square.classList.add('main-grid-cell')
         square.setAttribute('id', i)
         mainSudokuGrid.appendChild(square)
@@ -160,8 +150,8 @@ function createNumberBoard() {
     for (let i = 0; i < CONSTANT.GRID_SIZE; i++) {
         const numberBoard = document.createElement('div')
         numberBoard.classList.add('number')
-        numberBoard.setAttribute('id', i)
-        numberBoard.innerHTML = i
+        numberBoard.setAttribute('id', i + 1)
+        numberBoard.innerHTML = i + 1
         numberKeys.appendChild(numberBoard)
     }
     const deleteKey = document.createElement('div')
@@ -169,15 +159,248 @@ function createNumberBoard() {
     deleteKey.setAttribute('id', 'btn-delete')
     deleteKey.innerHTML = 'X'
     numberKeys.appendChild(deleteKey)
-
+    numberInputs = document.querySelectorAll('.number')
 
 }
 
 function initSudoku() {
+    const levelName = CONSTANT.LEVEL_NAME[level_index]
+    // clear old sudoku
+    clearSudoku()
+    resetBg()
     // generate sudoku puzzle
+    su = sudokuGen(level)
+    suAnswer = [...su.question]
+
+    console.log(suAnswer)
+    setSudokuToDiv()
+
 }
 
-//--------------------
+function setSudokuToDiv() {
+    //const cells = document.querySelectorAll('.main-grid-cell')
+    for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
+        let row = Math.floor(i / CONSTANT.GRID_SIZE)
+        let col = i % CONSTANT.GRID_SIZE
+        const divValue = suAnswer[row][col]
+        const imageHTML = `<img src='${tilePath}a${divValue}-${themeMode}.png' class="cell-image" alt="New Image">`
+
+        cells[i].setAttribute('data-value', divValue)
+
+        if (divValue !== 0) {
+            cells[i].classList.add('filled')
+            cells[i].innerHTML += imageHTML
+        }
+    }
+}
+
+function clearSudoku() {
+    for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
+        cells[i].innerHTML = ''
+        cells[i].classList.remove('filled')
+        cells[i].classList.remove('selected')
+    }
+}
+
+function returnStartScreen() {
+    clearInterval(timer)
+    pause = false
+    seconds = 0
+    startScreen.classList.add('active')
+    gameScreen.classList.remove('active')
+    pauseScreen.classList.remove('active')
+    gameTimer.innerHTML = '00:00:00'
+}
+
+function initGameGrid() {
+    //const cells = document.querySelectorAll('.main-grid-cell')
+
+    for (let i = 0; i < Math.pow(CONSTANT.GRID_SIZE, 2); i++) {
+        let row = Math.floor(i / CONSTANT.GRID_SIZE)
+        let col = i % CONSTANT.GRID_SIZE
+        if (row === 2 || row === 5) cells[i].style.marginBottom = '10px'
+        if (col === 2 || col === 5) cells[i].style.marginRight = '10px'
+
+    }
+}
+
+function hoverBg(index) {
+    let row = Math.floor(index / CONSTANT.GRID_SIZE)
+    let col = index % CONSTANT.GRID_SIZE
+
+    let boxStartRow = row - row % 3
+    let boxStartCol = col - col % 3
+
+    for (let i = 0; i < CONSTANT.BOX_SIZE; i++) {
+        for (let j = 0; j < CONSTANT.BOX_SIZE; j++) {
+            let cell = cells[9 * (boxStartRow + i) + (boxStartCol + j)]
+            cell.classList.add('hover')
+        }
+    }
+    let step = CONSTANT.GRID_SIZE
+    while (index - step >= 0) {
+        cells[index - step].classList.add('hover')
+        step += 9
+    }
+
+    step = 9
+    while (index + step < 81) {
+        cells[index + step].classList.add('hover')
+        step += 9
+    }
+
+    step = 1
+    while (index - step >= 9 * row) {
+        cells[index - step].classList.add('hover')
+        step += 1
+    }
+
+    step = 1
+    while (index + step < 9 * row + 9) {
+        cells[index + step].classList.add('hover')
+        step += 1
+    }
+}
+
+function resetBg() {
+    cells.forEach(e => e.classList.remove('hover'))
+}
+
+function initCellsEvent() {
+    cells.forEach((e, index) => {
+        e.addEventListener('click', () => {
+            if (!e.classList.contains('filled')) {
+                cells.forEach(e => e.classList.remove('selected'))
+
+                selectedCell = index
+                e.classList.remove('err')
+                e.classList.add('selected')
+                resetBg()
+                hoverBg(index)
+            }
+        })
+    })
+}
+
+function checkErr(value) {
+    const addErr = (cell) => {
+        if (parseInt(cell.getAttribute('data-value')) === value) {
+            cell.classList.add('err')
+            cell.classList.add('cell-err')
+            setTimeout(() => {
+                cell.classList.remove('cell-err')
+            }, 500)
+        }
+    }
+
+    let index = selectedCell
+
+    let row = Math.floor(index / CONSTANT.GRID_SIZE)
+    let col = index % CONSTANT.GRID_SIZE
+
+    let box_start_row = row - row % 3
+    let box_start_col = col - col % 3
+
+    for (let i = 0; i < CONSTANT.BOX_SIZE; i++) {
+        for (let j = 0; j < CONSTANT.BOX_SIZE; j++) {
+            let cell = cells[9 * (box_start_row + i) + (box_start_col + j)]
+            if (!cell.classList.contains('selected')) addErr(cell)
+        }
+    }
+
+    let step = 9
+    while (index - step >= 0) {
+        addErr(cells[index - step])
+        step += 9
+    }
+
+    step = 9
+    while (index + step < 81) {
+        addErr(cells[index + step])
+        step += 9
+    }
+
+    step = 1
+    while (index - step >= 9 * row) {
+        addErr(cells[index - step])
+        step += 1
+    }
+
+    step = 1
+    while (index + step < 9 * row + 9) {
+        addErr(cells[index + step])
+        step += 1
+    }
+}
+
+function removeErr() {
+    cells.forEach(e => e.classList.remove('err'))
+}
+
+function initNumberInputEvent() {
+    numberInputs.forEach((e, index) => {
+        e.addEventListener('click', () => {
+            if (!cells[selectedCell].classList.contains('filled')) {
+                cells[selectedCell].innerHTML = index + 1
+                cells[selectedCell].setAttribute('data-value', index + 1)
+                // add to answer
+                let row = Math.floor(selectedCell / CONSTANT.GRID_SIZE)
+                let col = selectedCell % CONSTANT.GRID_SIZE
+                suAnswer[row][col] = index + 1
+                // save game
+                saveGameInfo()
+                // -----
+                removeErr()
+                checkErr(index + 1)
+                cells[selectedCell].classList.add('zoom-in')
+                setTimeout(() => {
+                    cells[selectedCell].classList.remove('zoom-in')
+                }, 500)
+
+                // check game win
+                if (isGameWin()) {
+                    removeGameInfo()
+                    showResult()
+                }
+                // ----
+            }
+        })
+    })
+}
+
+function saveGameInfo() {
+    let game = {
+        level: level_index,
+        seconds: seconds,
+        su: {
+            original: su.original,
+            question: su.question,
+            answer: suAnswer
+        }
+    }
+    localStorage.setItem('game', JSON.stringify(game))
+}
+
+function removeGameInfo() {
+    localStorage.removeItem('game')
+    document.querySelector('#btn-continue').style.display = 'none'
+}
+
+function isGameWin() {
+    return sudokuCheck(suAnswer)
+}
+
+function showResult() {
+    clearInterval(timer)
+    resultScreen.classList.add('active')
+    resultTime.innerHTML = showTime(seconds)
+}
+
+//---------------------------
 
 init()
+
+
+
+
 
--- assets/js/sudoku.js
+++ assets/js/sudoku.js
@@ -2,7 +2,7 @@ function newGrid(size) {
     let arr = new Array(size)
 
     for (let i = 0; i < size; i++) {
-        arr[1] = new Array(size)
+        arr[i] = new Array(size)
     }
 
     for (let i = 0; i < Math.pow(size, 2); i++) {
@@ -10,4 +10,253 @@ function newGrid(size) {
     }
 
     return arr
+}
+
+//=============================
+
+//check duplicate number in col
+const isColSafe = (grid, col, value) => {
+    for (let row = 0; row < CONSTANT.GRID_SIZE; row++) {
+        if (grid[row][col] === value) return false
+    }
+    return true
+}
+//=============================
+
+//check duplicate number in row
+const isRowSafe = (grid, row, value) => {
+    for (let col = 0; col < CONSTANT.GRID_SIZE; col++) {
+        if (grid[row][col] === value) return false
+    }
+    return true
+}
+//=============================
+
+//check duplicate number in box-grid
+// check duplicate number in 3x3 box
+const isBoxSafe = (grid, box_row, box_col, value) => {
+    for (let row = 0; row < CONSTANT.BOX_SIZE; row++) {
+        for (let col = 0; col < CONSTANT.BOX_SIZE; col++) {
+            if (grid[row + box_row][col + box_col] === value) return false
+        }
+    }
+    return true
+}
+
+// check in row, col and 3x3 box
+const isSafe = (grid, row, col, value) => {
+    return isColSafe(grid, col, value) &&
+        isRowSafe(grid, row, value) &&
+        isBoxSafe(grid, row - row % 3, col - col % 3, value) &&
+        value !== CONSTANT.UNASSIGNED
+}
+//=============================
+
+// find unassigned cell
+const findUnassignedPos = (grid, pos) => {
+    for (let row = 0; row < CONSTANT.GRID_SIZE; row++) {
+        for (let col = 0; col < CONSTANT.GRID_SIZE; col++) {
+            if (grid[row][col] === CONSTANT.UNASSIGNED) {
+                pos.row = row
+                pos.col = col
+                return true
+            }
+        }
+    }
+    return false
+}
+//=============================
+
+// shuffle arr
+const shuffleArray = (arr) => {
+    let curr_index = arr.length
+
+    while (curr_index !== 0) {
+        let rand_index = Math.floor(Math.random() * curr_index)
+        curr_index -= 1
+
+        let temp = arr[curr_index]
+        arr[curr_index] = arr[rand_index]
+        arr[rand_index] = temp
+    }
+
+    return arr
+}
+//=============================
+
+// check puzzle is complete
+const isFullGrid = (grid) => {
+    return grid.every((row, i) => {
+        return row.every((value, j) => {
+            return value !== CONSTANT.UNASSIGNED
+        })
+    })
+}
+//=============================
+
+const sudokuCreate = (grid) => {
+    let unassigned_pos = {
+        row: -1,
+        col: -1
+    }
+
+    if (!findUnassignedPos(grid, unassigned_pos)) return true
+
+    let number_list = shuffleArray([...CONSTANT.NUMBERS])
+
+    let row = unassigned_pos.row
+    let col = unassigned_pos.col
+
+    number_list.forEach((num, i) => {
+        if (isSafe(grid, row, col, num)) {
+            grid[row][col] = num
+
+            if (isFullGrid(grid)) {
+                return true
+            } else {
+                if (sudokuCreate(grid)) {
+                    return true
+                }
+            }
+
+            grid[row][col] = CONSTANT.UNASSIGNED
+        }
+    })
+
+    return isFullGrid(grid)
+}
+//=============================
+
+const sudokuCheck = (grid) => {
+    let unassigned_pos = {
+        row: -1,
+        col: -1
+    }
+
+    if (!findUnassignedPos(grid, unassigned_pos)) return true
+
+    grid.forEach((row, i) => {
+        row.forEach((num, j) => {
+            if (isSafe(grid, i, j, num)) {
+                if (isFullGrid(grid)) {
+                    return true
+                } else {
+                    if (sudokuCreate(grid)) {
+                        return true
+                    }
+                }
+            }
+        })
+    })
+
+    return isFullGrid(grid)
+}
+//=============================
+
+const rand = () => Math.floor(Math.random() * CONSTANT.GRID_SIZE)
+//=============================
+
+const removeCells = (grid, level) => {
+    let res = [...grid]
+    let attemps = level
+    while (attemps > 0) {
+        let row = rand()
+        let col = rand()
+        while (res[row][col] === 0) {
+            row = rand()
+            col = rand()
+        }
+        res[row][col] = CONSTANT.UNASSIGNED
+        attemps--
+    }
+    return res
+}
+//=============================
+
+// generate sudoku base on level
+const sudokuGen = (level) => {
+    const boardNumber = 5 //Math.floor(Math.random() * 5000)
+    const puzzleBoard = sudokuPuzzle.find(puzzle => puzzle.id === boardNumber)
+    console.log(boardNumber)
+    console.log(level)
+    console.log('board', puzzleBoard.easy.solution)
+    let sudoku = breakDown(puzzleBoard.easy.solution)
+    let check = sudokuCreate(sudoku)
+    if (check) {
+        let question = breakDown(puzzleBoard.easy.board)
+        return {
+            original: sudoku,
+            question: question
+        }
+    }
+    return undefined
+}
+
+const breakDown = (board, type = 'number') => {
+    if (type === 'Alpha') {
+        const charMapping = {
+            1: 'A',
+            2: 'B',
+            3: 'C',
+            4: 'D',
+            5: 'E',
+            6: 'F',
+            7: 'G',
+            8: 'H',
+            9: 'I',
+            10: 'J',
+            11: 'K',
+            12: 'L'
+        }
+        let resultString = ''
+        for (let i = 0; i < board.length; i++) {
+            const currentChar = board[i]
+            const mappedChar = charMapping[currentChar] || currentChar // Use the mapped value or keep the original character
+            resultString += mappedChar
+        }
+        board = resultString
+    }
+    const newBoard = []
+    console.log('breakDown', board)
+    let temprow = []
+    for (let i = 0; i < board.length + 1; i++) {
+        if ((i + 1) % 9 === 0) {
+            if (i !== 0) {
+                temprow.push(parseInt(board[i]))
+                newBoard.push(temprow)
+                temprow = []
+            } else {
+                temprow.push(parseInt(board[i]))
+            }
+        } else {
+            temprow.push(parseInt(board[i]))
+        }
+    }
+    return newBoard
+}
+// this will determine if the board is letters or word or numbers
+const getBoardType = (board, type) => {
+}
+
+function shuffleString(str) {
+    // Convert the string to an array of characters
+    const arr = str.split('')
+
+    // Implement the Fisher-Yates shuffle algorithm
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        // Swap the characters at indices i and j
+        [arr[i], arr[j]] = [arr[j], arr[i]]
+    }
+
+    // Join the shuffled array back into a string
+    return arr.join('')
+}
+
+// Example usage:
+function testArray() {
+    const numString = '123456789'
+    const shuffledString = shuffleString(numString)
+    console.log(shuffledString)
+
 }
